6eb5a0e561dce9d5a55119921e6a329a06b7215d
George Cooper <coopsgeorge@gmail.com>
2020-03-08 11:58:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,6 +29,7 @@ emoticons.each do |meaning, array_values|
   english_emoticon = array_values[0]
   japenese_emoticon = array_values[1]
   emoticon_library['get_meaning'][japenese_emoticon] = meaning
+  emoticon_library['get_emoticon'][english_emoticon] = japenese_emoticon
 end
 
 
