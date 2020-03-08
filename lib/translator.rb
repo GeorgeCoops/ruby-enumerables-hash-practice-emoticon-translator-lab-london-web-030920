4bfb530adf26ad633325a06560d0c842d9768f2f
George Cooper <coopsgeorge@gmail.com>
2020-03-08 11:42:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,6 +2,20 @@
 require "yaml"
 require "pry"
 
+# => {"angel"=>["O:)", "☜(⌒▽⌒)☞"],
+#  "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"],
+#  "bored"=>[":O", "(ΘεΘ;)"],
+#  "confused"=>["%)", "(゜.゜)"],
+#  "embarrased"=>[":$", "(#^.^#)"],
+#  "fish"=>["><>", ">゜))))彡"],
+#  "glasses"=>["8D", "(^0_0^)"],
+#  "grinning"=>["=D", "(￣ー￣)"],
+#  "happy"=>[":)", "(＾ｖ＾)"],
+#  "kiss"=>[":*", "(*^3^)/~☆"],
+#  "sad"=>[":'(", "(Ｔ▽Ｔ)"],
+#  "surprised"=>[":o", "o_O"],
+#  "wink"=>[";)", "(^_-)"]}
+
 
 def load_library(emoticon_file)
 emoticons = YAML.load_file(emoticon_file)
