6aa931594b34cc19e94391ae03e33595779e2388
George Cooper <coopsgeorge@gmail.com>
2020-03-08 11:30:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,14 +5,14 @@ require "yaml"
 
 # def load_library(file)
 # emoticons_library = YAML.load_file('./lib/emoticons.yml')
-# 
+#
 # emoticons_library
 # end
-# 
+#
 # def get_japanese_emoticon
 #   # code goes here
 # end
-# 
+#
 # def get_english_meaning
 #   # code goes here
 # end
